f02a84fbca188e9c0a88fa00834c688850d768cf
BiBiOTA <yukiotataitien@gmail.com>
2022-07-07 00:17:23 +0800

    feat(title): Modify Article title to html content.

--- docs/.vitepress/theme/components/ArticleTitle.vue
+++ docs/.vitepress/theme/components/ArticleTitle.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <div class="border-2 rounded-md m-2">
-      <h1 class="article_title">
-        {{ frontmatter.title }}
-      </h1>
+      <h1
+        v-html="frontmatter.title"
+        class="article_title"
+      />
       <div class="article_date">
         {{ moment(frontmatter.date).format('YYYY-MM-DD') }}
       </div>
